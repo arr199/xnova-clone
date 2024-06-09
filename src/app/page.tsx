eff996f777e0db3d06fa501ac5b8253b925ff0ec
arr199 <abieledelso@gmail.com>
2024-06-09 15:12:53 +0100

    chore: Add authentication check to page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-import { MINES } from '@/entities/mines';
+import { auth } from '@/auth/auth';
+import { redirect } from 'next/navigation';
 
-export default function Home(): JSX.Element {
-	return (
-		<div className="flex justify-center p-24  w-full   ">
-			<div className="grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-10 w-full ">
-				{MINES.map(mine => (
-					<div key={mine.id} className="relative flex flex-col gap-2 p-4 border rounded-md h-52  ">
-						<div className="flex justify-between items-center">
-							<h1 className="text-2xl">{mine.name}</h1>
-							<p className="border px-4 py-1 rounded">{mine.level}</p>
-						</div>
-						<button className="absolute right-2 bottom-2 bg-green-700 px-6 py-2 rounded hover:opacity-80 ">
-							Upgrade Mine
-						</button>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+export default async function Page(): Promise<JSX.Element> {
+	const session = await auth();
+
+	if (session === null) {
+		return redirect('/signin');
+	} else return redirect('/home');
 }
