142a559e501ff22a499a92494a246002c55b282f
arr199 <abieledelso@gmail.com>
2024-06-09 15:12:31 +0100

    feat: Add not-found page component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Page(): JSX.Element {
+	return (
+		<div className="">
+			Nothing here
+			<Link href={'/'}>Go back</Link>
+		</div>
+	);
+}
