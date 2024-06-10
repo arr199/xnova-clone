d9cf2303e839135736c99d9edfba6a897686836b
arr199 <abieledelso@gmail.com>
2024-06-10 22:27:32 +0100

    feat: Implement useAuth hook for authentication

---
+++ src/hooks/useSession.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+export function useAuth(): any {
+	const [session, setSession] = useState<Session | null>(null);
+	const [user, setUser] = useState<User | null>(null);
+
+	useEffect(() => {
+		async function getUser(): Promise<void> {
+			const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/auth/getUser`);
+			const data: Session = await res.json();
+
+			console.log(data);
+			setSession(data);
+			setUser(data.user);
+		}
+
+		getUser().then().catch(console.error);
+	}, []);
+
+	function signOut(): void {
+		fetch(`${process.env.NEXT_PUBLIC_URL}/api/auth/signOut`).then().catch(console.error);
+		setSession(null);
+		setUser(null);
+	}
+
+	return { session, user, signOut };
+}
