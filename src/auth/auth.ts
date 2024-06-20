bea74ef6a705279d8f3d9779bfc5567c24955ac9
arr199 <abieledelso@gmail.com>
2024-06-20 22:30:28 +0100

    refactor: Update authentication route to handle signOut and getUser endpoints

---
+++ src/auth/auth.ts
@@ -0,0 +1,120 @@
+'use server';
+import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+import { supabase } from '../supabase/client';
+import { ERROR } from './errors';
+import bcrypt from 'bcrypt';
+import { NextResponse } from 'next/server';
+
+export interface signInWithCredentialsResponse {
+	error?: string;
+	success?: string;
+}
+
+export async function signInWithCredentials(
+	prevState: FormData,
+	formdata: FormData
+): Promise<signInWithCredentialsResponse> {
+	const { email, password } = Object.fromEntries(formdata) as Record<string, string>;
+
+	if (email === null || password === null) {
+		return { error: ERROR.INVALID_CREDENTIALS };
+	}
+
+	//  VALIDATE INPUT
+	if (typeof email !== 'string' && String(email).length > 30) {
+		return { error: ERROR.INVALID_EMAIL };
+	}
+
+	if (typeof password !== 'string' && String(password).length > 30) {
+		return { error: ERROR.INVALID_PASSWORD };
+	}
+
+	try {
+		const { data, error } = await supabase.from('users').select('*').eq('email', email);
+
+		if (error !== null) {
+			return { error: ERROR.SERVER_ERROR };
+		}
+
+		if (data?.length <= 0) {
+			return { error: ERROR.USER_NOT_FOUND };
+		}
+
+		// CHECK PASSWORD
+		const user = data[0];
+		const isValidPassword = await bcrypt.compare(password, user.password);
+
+		if (!isValidPassword) {
+			return { error: ERROR.INVALID_CREDENTIALS };
+		}
+		// CREATE THE SESSION
+		const session = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET ?? '', {
+			expiresIn: '10d'
+		});
+
+		const expires = new Date(Date.now() + 1000 * 1000);
+		cookies().set('session', session, { expires, httpOnly: true });
+
+		return { success: 'success' };
+	} catch (error) {
+		return { error: ERROR.SERVER_ERROR };
+	}
+}
+
+export async function signUpWithCredentials(formdata: FormData): Promise<any> {
+	const { email, password } = Object.fromEntries(formdata) as Record<string, string>;
+
+	if (email === null || password === null) {
+		return { error: ERROR.INVALID_CREDENTIALS };
+	}
+
+	//  VALIDATE INPUT
+	if (typeof email !== 'string' && String(email).length > 30) {
+		return { error: ERROR.INVALID_EMAIL };
+	}
+
+	if (typeof password !== 'string' && String(password).length > 30) {
+		return { error: ERROR.INVALID_PASSWORD };
+	}
+
+	// CHECK IF EMAIL EXISTS
+	try {
+		const { data, error } = await supabase.from('users').select('*').eq('email', email);
+
+		if (error !== null) {
+			return { error: ERROR.SERVER_ERROR };
+		}
+
+		if (data?.length > 0) {
+			return { error: ERROR.EMAIL_EXISTS };
+		}
+	} catch (error) {
+		return { error: ERROR.SERVER_ERROR };
+	}
+
+	// INSERT USER
+	const saltRounds = 10;
+	const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+	try {
+		const { error } = await supabase.from('users').insert({ email, password: hashedPassword });
+
+		if (error !== null) {
+			return { error: ERROR.SERVER_ERROR };
+		}
+
+		return NextResponse.redirect('/signin');
+	} catch (error) {
+		return { error: ERROR.SERVER_ERROR };
+	}
+}
+
+export async function getSession(): Promise<any> {
+	const session = cookies().get('session');
+	return session;
+}
+
+export async function signOut(): Promise<void> {
+	cookies().set('session', '', { expires: new Date(0) });
+}
