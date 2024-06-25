e8ebf5b4d449722af6674abe721a3d32a21755f4
arr199 <abieledelso@gmail.com>
2024-06-25 20:56:03 +0100

    refactor: Update mines.ts to include profitPerHour property and change name to label

--- src/entities/mines.ts
+++ src/entities/mines.ts
@@ -1,12 +1,14 @@
 export interface Mine {
 	id: number;
-	name: 'BITCOIN MINE' | 'ETHEREUM MINE' | 'USD MINE';
+	label: 'BITCOIN MINE' | 'ETHEREUM MINE' | 'USD MINE';
 	image?: string;
 	level: number;
+	profitPerHour?: number;
+	type: 'btc' | 'eth' | 'usd';
 }
 
 export const MINES: Mine[] = [
-	{ id: 1, name: 'BITCOIN MINE', image: '/images/bitcoin.png', level: 1 },
-	{ id: 2, name: 'ETHEREUM MINE', image: '/images/ethereum.png', level: 1 },
-	{ id: 3, name: 'USD MINE', image: '/images/dollar.png', level: 1 }
+	{ id: 1, type: 'btc', label: 'BITCOIN MINE', image: '/images/bitcoin.png', level: 1 },
+	{ id: 2, type: 'eth', label: 'ETHEREUM MINE', image: '/images/ethereum.png', level: 1 },
+	{ id: 3, type: 'usd', label: 'USD MINE', image: '/images/dollar.png', level: 1 }
 ];
