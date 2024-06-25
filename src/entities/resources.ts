4e3c76685c9ab7fabb1c53566f18c38eb3b9899d
arr199 <abieledelso@gmail.com>
2024-06-25 20:55:58 +0100

    refactor: Update resources.ts to use type annotations and consistent property names

--- src/entities/resources.ts
+++ src/entities/resources.ts
@@ -1,15 +1,16 @@
+import type { IconType } from 'react-icons';
 import { FaEthereum, FaDollarSign } from 'react-icons/fa';
 import { FaBitcoin } from 'react-icons/fa6';
 
 export interface Resource {
-	id: number;
-	name: string;
-	icon: JSX.Element;
+	type: string;
+	label: string;
+	icon: IconType;
 	color: string;
 }
 
-export const RESOURCES = [
-	{ name: 'BTC', icon: FaBitcoin, color: 'orange' },
-	{ name: 'ETH', icon: FaEthereum, color: 'skyBlue' },
-	{ name: 'USD', icon: FaDollarSign, color: 'green' }
+export const RESOURCES: Resource[] = [
+	{ type: 'btc', label: 'BTC', icon: FaBitcoin, color: 'orange' },
+	{ type: 'eth', label: 'ETH', icon: FaEthereum, color: 'skyBlue' },
+	{ type: 'usd', label: 'USD', icon: FaDollarSign, color: 'green' }
 ];
